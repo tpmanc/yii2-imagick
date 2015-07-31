086725d5934ed184d0bd3179c341918feae81d72
tpmanc <tpxtrime@mail.ru>
2015-07-31 22:52:52 +0300

    thumb and resize

---
+++ Imagick.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * @author Chukancev Nikita <tpxtrime@mail.ru>
+ */
+namespace tpmanc\imagick;
+
+/**
+ * Working with Imagemagick
+ */
+class Imagick
+{
+    private $image;
+
+    /**
+     * @param string $imagePath Path to image
+     */
+    public function __construct($imagePath)
+    {
+        $this->image = new \Imagick($imagePath);
+    }
+
+    /**
+     * Save result
+     * @param string $path Save path
+     * @return void
+     */
+    public function saveTo($path)
+    {
+        $this->image->writeImage($path);
+        $this->image->destroy();
+    }
+
+    /**
+     * Create thumbnail
+     * @param integer $width
+     * @param integer $height
+     * @return tpmanc\imagick\Imagick
+     */
+    public function makeThumb($width, $height)
+    {
+        if ($width >= $height) {
+            $this->image->thumbnailImage($width, 0);
+        } else {
+            $this->image->thumbnailImage(0, $height);
+        }
+        return $this;
+    }
+
+    /**
+     * Resize image
+     * @param integer $width
+     * @param integer $height
+     * @return tpmanc\imagick\Imagick
+     */
+    public function resizeImage($width, $height)
+    {
+        if ($width >= $height) {
+            $this->image->adaptiveResizeImage($width, 0);
+        } else {
+            $this->image->adaptiveResizeImage(0, $height);
+        }
+        return $this;
+    }
+}
